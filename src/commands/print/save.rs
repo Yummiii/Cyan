ed7c5963480f71cdd158b10082c6e36fee41be31
Yummi <yummi@zuraaa.com>
2025-07-24 01:35:40 -0300

    transaction!

--- src/commands/print/save.rs
+++ src/commands/print/save.rs
@@ -1,6 +1,7 @@
 use crate::database::{Conn, Repos};
 use chrono::{TimeZone, Utc};
 use clap::Parser;
+use sqlx::Acquire;
 use std::path::PathBuf;
 use tokio::fs;
 
@@ -14,8 +15,10 @@ pub async fn run(args: Args, mut conn: Conn) -> anyhow::Result<()> {
     let list = conn.screenshot().list_meta_saved(false).await?;
     let mut i = 0;
 
+    let mut tx = conn.begin().await?;
+
     for screenshot in list {
-        let screenshot = conn.screenshot().get(screenshot.id).await?;
+        let screenshot = tx.screenshot().get(screenshot.id).await?;
         let timestamp = Utc.timestamp_opt(screenshot.created_at, 0).unwrap();
 
         let file = args.destination.join(timestamp.format("%Y-%m").to_string());
@@ -24,11 +27,13 @@ pub async fn run(args: Args, mut conn: Conn) -> anyhow::Result<()> {
         let file = file.join(format!("{}.png", screenshot.created_at));
         fs::write(&file, screenshot.data).await?;
 
-        conn.screenshot().set_saved(screenshot.id, true).await?;
+        tx.screenshot().set_saved(screenshot.id, true).await?;
         println!("Saved screenshot [{}] to {}", screenshot.id, file.display());
 
         i += 1;
     }
+
+    tx.commit().await?;
 
     println!("Saved {i} screenshots");
 
